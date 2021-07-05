a1112170de38a28a02b3ede03a675ec3b3e6f5c9
AminahShentour <aminah.shentour@revature.net>
2021-07-04 23:18:08 -0500

    Day 5

---
+++ Lab-StringMethods/src/StringMethods.java
@@ -0,0 +1,14 @@
+
+public class StringMethods {
+
+	public static void main(String[] args) {
+		String str ="Hello";
+		System.out.println(str.equals("Hello"));
+		
+		//use the length() method
+		//System.out.println(str.length()); 
+		
+		//use of indexOf()
+		System.out.println(str.indexOf("o"));
+	}
+}
